7049e2e2eeb1899235a41d91eba6bb0018cf3ccd
yukkop <hectic.yukkop@gmail.com>
2025-06-12 01:51:24 +0000

    refactor: postgresql module

--- flake.nix
+++ flake.nix
@@ -50,36 +50,38 @@
               (modulesPath + "/profiles/qemu-guest.nix")
             ];
           })
-          self.nixosModules.postgresqlService
+          self.nixosModules.postgresqlPreConfigure
           {
-            hectic.postgres.enable = true;
-            hectic.postgres.package = pkgs.postgresql_15;
-            hectic.postgres.extensions = {
-              pg_cron = true;
-              pgjwt = true;
-              pg_net = true;
-              pg_smtp_client = true;
-              http = true;
-	      hemar = true;
-            };
+            services.postgresql = {
+	      enable = true;
+              package = pkgs.postgresql_15;
+              lazzyExtensions = {
+                pg_cron = true;
+                pgjwt = true;
+                pg_net = true;
+                pg_smtp_client = true;
+                http = true;
+	        hemar = true;
+              };
 
-            hectic.postgres.authPreset = "allMixed";
+              authentication = self.lib.authPresets.allMixed;
 
-            # Provide an initial script if needed:
-            hectic.postgres.initialScript = pkgs.writeText "init-sql-script" ''
-              CREATE EXTENSION IF NOT EXISTS "http";
-                     \set gateway_schema `echo $GATEWAY_SCHEMA`
-                     SELECT :'gateway_schema' = ''' AS is_gateway_schema;
-                     \gset
-                     \if :is_gateway_schema
-                       \echo 'Error: Environment variable GATEWAY_SCHEMA is not set.'
-                       \quit 1
-                     \endif
+              # Provide an initial script if needed:
+              initialScript = pkgs.writeText "init-sql-script" ''
+                CREATE EXTENSION IF NOT EXISTS "http";
+                       \set gateway_schema `echo $GATEWAY_SCHEMA`
+                       SELECT :'gateway_schema' = ''' AS is_gateway_schema;
+                       \gset
+                       \if :is_gateway_schema
+                         \echo 'Error: Environment variable GATEWAY_SCHEMA is not set.'
+                         \quit 1
+                       \endif
 
-              CREATE SCHEMA :gateway_schema;
+                CREATE SCHEMA :gateway_schema;
 
-              ALTER DATABASE postgres SET "app.gateway" TO :'gateway_schema';
-            '';
+                ALTER DATABASE postgres SET "app.gateway" TO :'gateway_schema';
+              '';
+	    };
           }
           {
             environment.systemPackages = with pkgs; [git curl neovim postgresql_15];
@@ -126,13 +128,7 @@
       };
     })
     // {
-      nixosModules.postgresqlService = {
-        lib,
-        config,
-        pkgs,
-        ...
-      }: let
-        system = pkgs.system;
+      lib = {
         authPresets = {
           localTrusted = builtins.concatStringsSep "\n" [
             "local all       all     trust"
@@ -151,112 +147,166 @@
             "host  sameuser    all     127.0.0.1/32 scram-sha-256"
             "host  sameuser    all     ::1/128 scram-sha-256"
           ];
+	};
+      };
+      nixosModules = let
+        extensionFlags = {
+           pg_cron = false;
+           pgjwt = false;
+           pg_net = false;
+           pg_smtp_client = false;
+           http = false;
+           hemar = false;
         };
-        cfg = config.hectic.postgres;
       in {
-        options = {
-          hectic.postgres = {
-            enable = lib.mkOption {
-              type = lib.types.bool;
-              default = false;
-            };
-            authPreset = lib.mkOption {
-              type = lib.types.enum (lib.attrNames authPresets);
-              default = "localhostOnly";
-              description = "Which authentication preset to use for PostgreSQL (e.g. localTrusted, allMixed, localhostOnly).";
-            };
-            package = lib.mkOption {
-              type = lib.types.package;
-              default = pkgs.postgresql_16;
-            };
-            enableTCPIP = lib.mkOption {
-              type = lib.types.bool;
-              default = true;
-            };
-            extensions = lib.mkOption {
-              type = lib.types.attrsOf lib.types.bool;
-              default = {
-                pg_cron = false;
-                pgjwt = false;
-                pg_net = false;
-                pg_smtp_client = false;
-                http = false;
-		hemar = false;
+        postgresqlPreConfigure = { pkgs, config, lib, ... }: let
+          system = pkgs.system;
+          cfg = config.services.postgresql;
+        in {
+          options = {
+            services.postgresql = {
+              lazzyExtensions = lib.mkOption {
+                type = lib.types.attrsOf lib.types.bool;
+                default = extensionFlags;
               };
+	    };
+          };
+          config = lib.mkIf cfg.enable {
+            services.postgresql = {
+	      settings.shared_preload_libraries =
+                lib.concatStringsSep ", "
+                  (lib.attrNames (
+          	    lib.filterAttrs (n: v: v && 
+          	         n != "http" 
+          	      && n != "pgjwt" 
+          	      && n != "pg_smtp_client"
+          	  ) cfg.lazzyExtensions));
+
+              extensions = let
+                packages = {
+                  inherit (cfg.package.pkgs) pg_net pgjwt pg_cron http pg_smtp_client;
+                  hemar = util-hemar.packages.${system}.pg-15-hemar;
+                };
+              in
+                lib.attrValues (
+                  lib.filterAttrs (n: v: v != null)
+                  (lib.mapAttrs' (
+                      name: enabled:
+                        if enabled
+                        then lib.nameValuePair name (packages.${name} or (throw "Package ${name} not found in pkgs"))
+                        else null
+                    )
+                    cfg.lazzyExtensions)
+                );
             };
-            initialScript = lib.mkOption {
-              type = lib.types.nullOr lib.types.path;
-              default = null;
-            };
-            settings = lib.mkOption {
-              type = lib.types.attrs;
-              default = {};
-            };
-            host = lib.mkOption {
-              type = lib.types.string;
-              default = "127.0.0.1";
-              description = "Address to run PostgreSQL on";
-            };
-            port = lib.mkOption {
-              type = lib.types.int;
-              default = 5432;
-              description = "Port to run PostgreSQL on";
-            };
-            # passwordFile = lib.mkOption {
-            #   type = lib.types.path;
-            #   description = "Password for `postgres` user";
-            # };
           };
         };
-
-        config = lib.mkIf cfg.enable {
-          # systemd.services = lib.mkMerge [
-          #   {
-          #     postgresql.serviceConfig.Environment =
-          #       builtins.map (name: "${name}=${cfg.environment.${name}}")
-          #       (lib.attrNames cfg.environment);
-          #   }
-          # ];
-          services.postgresql = {
-            enable = true;
-            package = cfg.package;
-            enableTCPIP = cfg.enableTCPIP;
-
-	    /* postgresql.config */
-            settings = lib.mkMerge [
-              {
-                port = cfg.port;
-                listen_addresses = lib.mkOverride 80 cfg.host;
-                shared_preload_libraries =
-                  lib.concatStringsSep ", "
-                  (lib.attrNames (
-		    lib.filterAttrs (n: v: v && 
-		         n != "http" 
-		      && n != "pgjwt" 
-		      && n != "pg_smtp_client"
-		  ) cfg.extensions));
-              }
-	      /* additional custom settings */
-              cfg.settings
-            ];
-            extensions = let
-              packages = {
-                inherit (cfg.package.pkgs) pg_net pgjwt pg_cron http pg_smtp_client;
-		hemar = util-hemar.packages.${system}.pg-15-hemar;
+        postgresqlServiceLegacy = {
+          lib,
+          config,
+          pkgs,
+          ...
+        }: let
+          system = pkgs.system;
+          cfg = config.hectic.postgres;
+        in {
+          options = {
+            hectic.postgres = {
+              enable = lib.mkOption {
+                type = lib.types.bool;
+                default = false;
               };
-            in
-              lib.attrValues (
-                lib.filterAttrs (n: v: v != null)
-                (lib.mapAttrs' (
-                    name: enabled:
-                      if enabled
-                      then lib.nameValuePair name (packages.${name} or (throw "Package ${name} not found in pkgs"))
-                      else null
-                  )
-                  cfg.extensions)
-              );
-            authentication = lib.mkOverride 10 authPresets.${cfg.authPreset};
-            initialScript = cfg.initialScript;
+              authPreset = lib.mkOption {
+                type = lib.types.enum (lib.attrNames self.lib.authPresets);
+                default = "localhostOnly";
+                description = "Which authentication preset to use for PostgreSQL (e.g. localTrusted, allMixed, localhostOnly).";
+              };
+              package = lib.mkOption {
+                type = lib.types.package;
+                default = pkgs.postgresql_16;
+              };
+              enableTCPIP = lib.mkOption {
+                type = lib.types.bool;
+                default = true;
+              };
+              extensions = lib.mkOption {
+                type = lib.types.attrsOf lib.types.bool;
+                default = extensionFlags;
+              };
+              initialScript = lib.mkOption {
+                type = lib.types.nullOr lib.types.path;
+                default = null;
+              };
+              settings = lib.mkOption {
+                type = lib.types.attrs;
+                default = {};
+              };
+              host = lib.mkOption {
+                type = lib.types.string;
+                default = "127.0.0.1";
+                description = "Address to run PostgreSQL on";
+              };
+              port = lib.mkOption {
+                type = lib.types.int;
+                default = 5432;
+                description = "Port to run PostgreSQL on";
+              };
+              # passwordFile = lib.mkOption {
+              #   type = lib.types.path;
+              #   description = "Password for `postgres` user";
+              # };
+            };
+          };
+
+          config = lib.mkIf cfg.enable {
+            # systemd.services = lib.mkMerge [
+            #   {
+            #     postgresql.serviceConfig.Environment =
+            #       builtins.map (name: "${name}=${cfg.environment.${name}}")
+            #       (lib.attrNames cfg.environment);
+            #   }
+            # ];
+            services.postgresql = {
+              enable = true;
+              package = cfg.package;
+              enableTCPIP = cfg.enableTCPIP;
+
+              /* postgresql.config */
+              settings = lib.mkMerge [
+                {
+                  port = cfg.port;
+                  listen_addresses = lib.mkOverride 80 cfg.host;
+                  shared_preload_libraries =
+                    lib.concatStringsSep ", "
+                    (lib.attrNames (
+          	    lib.filterAttrs (n: v: v && 
+          	         n != "http" 
+          	      && n != "pgjwt" 
+          	      && n != "pg_smtp_client"
+          	  ) cfg.extensions));
+                }
+                /* additional custom settings */
+                cfg.settings
+              ];
+              extensions = let
+                packages = {
+                  inherit (cfg.package.pkgs) pg_net pgjwt pg_cron http pg_smtp_client;
+          	hemar = util-hemar.packages.${system}.pg-15-hemar;
+                };
+              in
+                lib.attrValues (
+                  lib.filterAttrs (n: v: v != null)
+                  (lib.mapAttrs' (
+                      name: enabled:
+                        if enabled
+                        then lib.nameValuePair name (packages.${name} or (throw "Package ${name} not found in pkgs"))
+                        else null
+                    )
+                    cfg.extensions)
+                );
+              authentication = lib.mkOverride 10 self.lib.authPresets.${cfg.authPreset};
+              initialScript = cfg.initialScript;
+            };
           };
         };
       };
